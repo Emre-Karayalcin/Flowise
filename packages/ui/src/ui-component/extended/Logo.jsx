d8663c1e1b3449748e4a357194eb3ec5e8441167
Steven <nguyensy15121997@gmail.com>
2025-07-27 19:32:18 +0700

    Change icon

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from '@/assets/images/nuggets-light-logo.png'
-import logoDark from '@/assets/images/nuggets-dark-logo.png'
+import logo from '@/assets/images/nuggets-dark-logo.png'
+import logoDark from '@/assets/images/nuggets-light-logo.png'
 
 import { useSelector } from 'react-redux'
 
